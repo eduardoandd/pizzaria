07599af49e3880522da183144844ba64fc6d5da4
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-11-17 21:24:39 -0300

    refazendo método total price

--- pizzaria/orders_mongo/models.py
+++ pizzaria/orders_mongo/models.py
@@ -1,9 +1,14 @@
+from decimal import Decimal
 from django.db import models
 
 from products_mongo.models import ProductMongo
 
 class OrderMongo(models.Model):
     Product = models.ManyToManyField(ProductMongo, related_name='orders')
+    
+    def total_price(self):
+        total = sum(Decimal(product.Price.to_decimal())for product in self.Product.all())
+        return total
     
     
     def __str__(self):
--- pizzaria/orders_mongo/serializers.py
+++ pizzaria/orders_mongo/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from orders_mongo.models import OrderMongo
 
 class OrderMongoSerializer(serializers.ModelSerializer):
-    totalprice = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = OrderMongo
         fields='__all__'
     
+    def get_total_price(self, obj):
+        return obj.total_price()
 
     
